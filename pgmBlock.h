4f99c67308e5c03873de5bfb5d82f64c973f012d
Sam Kristoff <sam.kristoff@digilent.com>
2017-01-18 22:03:18 +0000

    Add source files and submodules

---
+++ pgmBlock.h
@@ -0,0 +1,19 @@
+#ifndef PGMBLOCK_H
+#define PGMBLOCK_H
+
+#include <QByteArray>
+
+
+class PgmBlock
+{
+public:
+    PgmBlock();
+    bool operator ==(const PgmBlock &other);
+    bool operator <(const PgmBlock &other);
+
+    unsigned int address;
+    QByteArray data;
+
+};
+
+#endif // PGMBLOCK_H
